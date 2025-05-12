b916b948b3fe804b25d2b8db7233a304c4592c34
kyrem_ortiz <kyremortizgomez@hotmail.com>
2025-05-12 20:35:20 +0900

    init

---
+++ demo/src/main/java/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+    public static void main(String[] args) {
+
+        // add our code here
+        System.out.println("Hello World of Java!");
+        System.out.println("My favorite color is green.");
+        System.out.println("My hobbie is coding.");
+
+    }
+
+}
